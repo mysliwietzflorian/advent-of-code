d9d5865bc29bd19c2c26aa1ce8a0f1a28ebb2829
Florian Mysliwietz <myslflo1996@yahoo.de>
2022-12-05 18:18:52 +0100

    commit day 5

---
+++ 2022/05/b.py
@@ -0,0 +1,38 @@
+import re
+
+def main():
+    f = open('input.txt', 'r')
+    lines = f.read().split('\n\n')
+    crates_input = lines[0].split('\n')
+    moves_input = lines[1].split('\n')
+
+    stacks = init_stacks(crates_input)
+    calc_moves(moves_input, stacks)
+
+    solution = ''
+    for s in stacks:
+        solution += s.pop()
+    print(solution)
+
+def init_stacks(crates_input):
+    
+    nr_of_stacks = len(re.findall(r'\d+', crates_input[-1]))
+    stacks = [[] for _ in range(nr_of_stacks)]
+
+    for crateLine in reversed(crates_input[:-1]):
+        for index in range(len(stacks)):
+            curr = crateLine[4*index + 1]
+            if curr != ' ':
+                stacks[index].append(curr)
+    return stacks
+
+def calc_moves(moves_input, stacks):
+    for move_line in moves_input:
+        move, from_stack, to_stack = [int(i) for i in re.findall(r'\d+', move_line)]
+
+        values = stacks[from_stack - 1][-move:]
+        stacks[to_stack - 1].extend(values)
+        del stacks[from_stack - 1][-move:]
+
+if __name__ == '__main__':
+    main()
