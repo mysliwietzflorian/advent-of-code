cf55da7270c315d1bab54013bcdb3795dec1aecf
Florian Mysliwietz <myslflo1996@yahoo.de>
2023-12-09 08:27:22 +0100

    add day 09 (2023)

---
+++ 2023/09/a.py
@@ -0,0 +1,28 @@
+import re
+
+with open ('input.txt') as f:
+    lines = f.read().splitlines()
+
+def is_empty(list):
+    for i in list:
+        if i != 0:
+            return False
+    return True
+
+sum = 0
+for line in lines:
+    sequence = list(map(int, re.findall('([\d-]+)', line)))
+
+    stack = []
+    current = sequence
+    while not is_empty(current):
+        stack.append(current)
+        diff = [current[i + 1] - current[i] for i in range(len(current) - 1)]
+        current = diff
+
+    value = 0
+    for i in reversed(range(len(stack))):
+        value += stack[i][-1]
+    sum += value
+
+print(sum)
