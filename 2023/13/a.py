470d37800a477619b53e0edccc73e7d481cb5ff6
Florian Mysliwietz <myslflo1996@yahoo.de>
2023-12-13 17:42:37 +0100

    add day 13 (2023)

---
+++ 2023/13/a.py
@@ -0,0 +1,38 @@
+f = open('input.txt', 'r')
+input = f.read().split('\n\n')
+
+def transpose(grid):
+    result = [[] for _ in range(len(grid[0]))]
+    for i in range(len(grid[0])):
+        result[i] = [row[i] for row in grid]
+    
+    return result
+
+def find_row_reflection(grid):
+    grid = transpose(grid)
+    return find_col_reflection(grid)
+
+def find_col_reflection(grid):
+    for i in range(len(grid) - 1):
+        j = i + 1
+        offset = 0
+
+        mirrored = grid[i] == grid[j]
+        while mirrored and i - offset > 0 and j + offset + 1 < len(grid):
+            offset += 1
+            mirrored = grid[i - offset] == grid[j + offset]
+
+        if mirrored:
+            return i + 1
+
+    return 0
+
+sum = 0
+for g in input:
+    grid = [i for i in g.split('\n') if i]
+
+    rows = find_row_reflection(grid)
+    cols = find_col_reflection(grid)
+    sum += cols * 100 + rows
+
+print(sum)
